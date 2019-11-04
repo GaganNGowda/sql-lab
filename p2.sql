42107bdafdc3e33c13f79bc4b486c106b2bd3b55
Gagan Gowda N <54549724+GaganNGowda@users.noreply.github.com>
2019-11-04 15:12:27 +0530

    Add files via upload

---
+++ p2.sql
@@ -0,0 +1,14 @@
+DECLARE
+	a integer:=10;
+	b integer:=20;
+	c integer;
+	f real;
+	
+BEGIN
+	c:=a+b;
+	dbms_output.put_line('value of c is'|| c);
+	f:=70.0/3.0;
+	
+	dbms_output.put_line('value of f is'|| f);
+END;
+/ 
